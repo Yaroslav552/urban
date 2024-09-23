3d2a1dba5e6e3488d29648887548cb7ca01db92a
Yaroslav552 <boywithoutmood@gmail.com>
2024-09-23 20:02:13 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 immutable_var = ([1, 2], '3', '4')
 print(immutable_var)
-mutable_list = ([1, 2] ,'4', '3')
-print(mutable_list)
-mutable_list = ([1, 2] ,'4', '3') * 3
+mutable_list = ["красный", "синий", "белый", "черный"]
+mutable_list[1] = "черный"
 print(mutable_list)
