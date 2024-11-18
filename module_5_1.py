40ef33b22f71ba28b9e467f2668d4e53550015df
Yaroslav552 <boywithoutmood@gmail.com>
2024-11-18 20:25:57 +0300

    Add files via upload

--- module_5_1.py
+++ module_5_1.py
@@ -1,3 +1,6 @@
+from zoneinfo import reset_tzpath
+
+
 class House:
     def __init__(self, name, number_of_floors):
         self.name = name
@@ -8,9 +11,17 @@ class House:
                 print(f'{floor}')
             else:
                 print('Такого этажа не существует')
-h1 = House('ЖК Горский', 18)
-h2 = House('Домик в деревне', 2)
-h1.go_to(5)
-h2.go_to(10)
+    def __len__(self):
+        return self.number_of_floors
+    def __str__(self):
+        title = str(f'Название: {self.name}, кол-во этажей: {self.number_of_floors}')
+        return title
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+print(h1)
+print(h2)
+print(len(h1))
+print(len(h2))
 
 
